d844dd509c960437647065f4e1345e69ab174eea
Yannick Menager <ymenager@kloudtek.com>
2015-03-23 23:40:45 +0000

    Automatically retry to create db if connection times out or unable to resolve

--- src/main/java/com/kloudtek/confluenceconfig/CreateDatabase.java
+++ src/main/java/com/kloudtek/confluenceconfig/CreateDatabase.java
@@ -6,7 +6,10 @@ package com.kloudtek.confluenceconfig;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
+import org.postgresql.util.PSQLException;
 
+import java.net.ConnectException;
+import java.net.UnknownHostException;
 import java.sql.*;
 
 /**
@@ -30,23 +33,47 @@ public class CreateDatabase {
     private String confluenceDbPassword;
     @Parameter(names = "-cn", description = "Confluence database name")
     private String confluenceDbName = "confluence";
+    @Parameter(names = {"-r", "--retry"}, description = "Number of seconds to keep trying to connect or resolve the server")
+    private long retry = 600;
+    @Parameter(names = {"-rg", "--retrygap"}, description = "Gap between retries (in seconds)")
+    private int retryGap = 10;
 
     public void execute() throws SQLException {
-        try (Connection db = DriverManager.getConnection("jdbc:postgresql://" + dbAddress + ":" + dbPort + "/" + masterDbName, dbMasterUsername, dbMasterPassword)) {
-            try (Statement st = db.createStatement()) {
-                ResultSet rs = st.executeQuery("SELECT 1 AS result FROM pg_database WHERE datname='" + confluenceDbName + "'");
-                if (!rs.next()) {
-                    st.execute("CREATE ROLE " + confluenceDbUsername + " WITH LOGIN PASSWORD '" + confluenceDbPassword + "' valid until 'infinity';");
-                    st.execute("CREATE ROLE tmpusers NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;");
-                    st.execute("GRANT tmpusers TO " + dbMasterUsername + ";\n");
-                    st.execute("GRANT " + confluenceDbUsername + " TO " + dbMasterUsername + ";\n");
-                    st.execute("CREATE DATABASE " + confluenceDbName + " WITH ENCODING='UTF8' OWNER=" + confluenceDbUsername + " connection limit=-1;");
-                    st.execute("REVOKE ADMIN OPTION FOR tmpusers FROM " + dbMasterUsername + ";\n");
-                    st.execute("REVOKE ADMIN OPTION FOR tmpusers FROM " + confluenceDbUsername + ";\n");
-                    st.execute("DROP ROLE tmpusers\n");
-                    System.out.println("Database " + confluenceDbName + " created");
+        boolean retry = true;
+        long timeout = System.currentTimeMillis() + (this.retry * 1000L);
+        while (retry) {
+            String url = "jdbc:postgresql://" + dbAddress + ":" + dbPort + "/" + masterDbName;
+            System.out.println("Creating database on server " + url);
+            try (Connection db = DriverManager.getConnection(url, dbMasterUsername, dbMasterPassword)) {
+                try (Statement st = db.createStatement()) {
+                    ResultSet rs = st.executeQuery("SELECT 1 AS result FROM pg_database WHERE datname='" + confluenceDbName + "'");
+                    if (!rs.next()) {
+                        st.execute("CREATE ROLE " + confluenceDbUsername + " WITH LOGIN PASSWORD '" + confluenceDbPassword + "' valid until 'infinity';");
+                        st.execute("CREATE ROLE tmpusers NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;");
+                        st.execute("GRANT tmpusers TO " + dbMasterUsername + ";\n");
+                        st.execute("GRANT " + confluenceDbUsername + " TO " + dbMasterUsername + ";\n");
+                        st.execute("CREATE DATABASE " + confluenceDbName + " WITH ENCODING='UTF8' OWNER=" + confluenceDbUsername + " connection limit=-1;");
+                        st.execute("REVOKE ADMIN OPTION FOR tmpusers FROM " + dbMasterUsername + ";\n");
+                        st.execute("REVOKE ADMIN OPTION FOR tmpusers FROM " + confluenceDbUsername + ";\n");
+                        st.execute("DROP ROLE tmpusers\n");
+                        System.out.println("Database " + confluenceDbName + " created");
+                    } else {
+                        System.out.println("Database " + confluenceDbName + " already exists");
+                    }
+                }
+                retry = false;
+            } catch (PSQLException e) {
+                Throwable cause = e.getCause();
+                boolean transientError = cause != null && (cause instanceof UnknownHostException || cause instanceof ConnectException);
+                if (!transientError || System.currentTimeMillis() > timeout) {
+                    throw e;
                 } else {
-                    System.out.println("Database " + confluenceDbName + " already exists");
+                    System.out.println("Unable to connect to server (retrying in " + retryGap + " seconds): " + cause);
+                    try {
+                        Thread.sleep(retryGap * 1000L);
+                    } catch (InterruptedException e1) {
+                        return;
+                    }
                 }
             }
         }
